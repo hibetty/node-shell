d6c8fbe121105221140a5f527cbcacf01da97cf6
Betty Lin <bettyhappylin@gmail.com>
2017-04-24 14:29:11 -0400

    clean up file spacing

--- bash.js
+++ bash.js
@@ -1,9 +1,13 @@
 var command = require('./commands');
-const chalk = require('chalk');
+var chalk = require('chalk');
+
+var done = function(output){
+  process.stdout.write(output);
+  process.stdout.write(chalk.yellow('\nprompt > '));
+};
+
 //output a prompt
 process.stdout.write(chalk.yellow('prompt > '));
-
-
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(){
@@ -11,12 +15,5 @@ process.stdin.on('data', function(){
   var cmd = args.slice(0, args.indexOf(' '));
   var body = args.slice(args.indexOf(' ')).trim();
 
-command[cmd](body, done);
-
+  command[cmd](body, done);
 });
-
-var done = function(output){
-  process.stdout.write(output);
-  process.stdout.write(chalk.yellow('\nprompt > '));
-};
-
