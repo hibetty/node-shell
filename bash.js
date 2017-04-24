312f08ca22b36d47a0aa46bb9c87ab86aefa31d0
Betty Lin <bettyhappylin@gmail.com>
2017-04-24 13:07:35 -0400

    add more functions, finish workshop [excluding bonus]

--- bash.js
+++ bash.js
@@ -1,13 +1,22 @@
+var command = require('./commands');
+const chalk = require('chalk');
 //output a prompt
-process.stdout.write('prompt > ');
+process.stdout.write(chalk.yellow('prompt > '));
+
+
 
 //The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', function(data){
-  var cmd = data.toString().trim(); //remove the newline
+process.stdin.on('data', function(){
+  var args = String(...arguments);
+  var cmd = args.slice(0, args.indexOf(' '));
+  var body = args.slice(args.indexOf(' ')).trim();
 
-  if(cmd === 'date'){
-    const now = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') ;
+command[cmd](body, done);
 
-    process.stdout.write(now);
-  }
-})
+});
+
+var done = function(output){
+  process.stdout.write(output);
+  process.stdout.write(chalk.yellow('\nprompt > '));
+};
+
--- commands.js
+++ commands.js
@@ -1,7 +1,62 @@
+var fs = require('fs');
+var chalk = require('chalk');
+var output;
 
-if(cmd === 'pwd'){
-  process.stdout.write(__dirname);
-}
+module.exports = {
+  pwd: function(param, done){
+  output = chalk.magenta.bold(__dirname);
+  done(output);
+  },
+  date: function(param, done){
+    const now = new Date().toISOString().replace(/T/, ' ')
+      .replace(/\..+/, '');
+    output = chalk.red.underline(now);
+    done(output);
+  },
+  ls: function(param, done){
+    var files = fs.readdir(__dirname, function(err, files){
+    if (err) throw err;
+    output = chalk.green(files.join('     '));
+    done(output);
+    });
+  },
+  echo: function(param, done){
+    output = chalk.cyan.italic(String(param));
+    done(output);
+  },
+  cat: function(param, done){
+    var path = __dirname + '/' + param;
+    fs.readFile(path, 'utf8', function(err, data){
+      if (err) throw err;
+      output = data;
+      done(output);
+    });
+  },
+  head: function(param, done) {
+    var path = __dirname + '/' + param;
+    var theHead = '';
 
-var prompt = process.stdout.write('\nprompt > ');
+    fs.readFile(path, 'utf8', function(err, data){
+      if (err) throw err;
+      var arr = data.split('\n');
+      for (var i = 0; i < 5; i++){
+        theHead += arr[i] + '\n';
+      }
+      done(theHead);
+    });
+  },
+  tail: function(param, done) {
+    var path = __dirname + '/' + param;
+    var theTail = '';
 
+    fs.readFile(path, 'utf8', function(err, data){
+      if (err) throw err;
+      var arr = data.split('\n');
+      var len = arr.length;
+      for (var i = len - 5; i < len; i++){
+        theTail += arr[i] + '\n';
+      }
+      done(theTail);
+    });
+  }
+};
